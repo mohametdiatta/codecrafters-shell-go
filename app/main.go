67ba48f8fbe0ce344b9c7b23e106cdc4ffbc750d
mohamet <mohamed.diatta@paps-app.com>
2026-05-08 15:14:37 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -110,7 +110,7 @@ func evalCommand(args []string) {
 
 		args = args[:len(args)-2]
 	}
-	if len(args) > 2 && args[len(args)-2] == ">>" {
+	if len(args) > 2 && (args[len(args)-2] == ">>" || args[len(args)-2] == "1>>") {
 		outputFile, err := os.OpenFile(args[len(args)-1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			panic(err)
